475ba3061eda8c5cdc57a16d8f775e5aa5da153d
João <joaokz@outlook.com>
2025-04-18 20:18:46 -0300

    First Commit.

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:gym_dev/screens/home/home_screen.dart';
+
+void main() {
+  runApp(MaterialApp(home: HomeScreen()));
+}
---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  final primaryColor = Color(0xFF45EA69);
+  final white = Color(0xFFFFFFFF);
+
+  final dark900 = Color(0xFF1E1E1E);
+  final dark800 = Color(0xFF232323);
+  final dark700 = Color(0xFF252525);
+  final dark600 = Color(0xFF272727);
+  final dark500 = Color(0xFF2C2C2C);
+  final dark400 = Color(0xFF2E2E2E);
+  final dark300 = Color(0xFF333333);
+  final dark200 = Color(0xFF363636);
+  final dark100 = Color(0xFF383838);
+}
